56f6e0cd9cc265fd767f363e1159456ab5ac91b4
GHOST <kenryalonzo@gmail.com>
2023-12-27 08:53:27 +0100

    fix: I have just finished with the GUIs of my project and now I am moving on to the logic of the code in another theme the backend.

---
+++ consult.py
@@ -0,0 +1,8 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+
+Builder.load_file("consult.kv")
+
+
+class ConsultBmi(BoxLayout):
+    pass
---
+++ update_page.py
@@ -0,0 +1,7 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+
+Builder.load_file("update_page.kv")
+
+class UpdatePage(BoxLayout):
+    pass
